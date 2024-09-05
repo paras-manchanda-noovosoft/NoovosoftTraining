f3af7f365e88316588806c75b9d5ae95416a1b5b
parasmanchanda <paras.manchanda@noovosoft.com>
2024-09-05 19:26:29 +0530

    Typescript number to roman exercise

--- ReactExercise/react-product/src/App.js
+++ ReactExercise/react-product/src/App.js
@@ -1,15 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
-import Home2 from "./Components/home2.";
-import Home from "./Components/home";
+import CartPage from "./Components/cartPage";
+import HomePage from "./Components/homePage";
+import Cartstore from "./Components/cartstore";
+import {observer} from "mobx-react";
+import {BrowserRouter as Router, Route, Link, Routes} from "react-router-dom";
+import ProductStore from "./Components/productStore";
 
-function App() {
-  return (
-    <div className="App">
-        {/*<Home />*/}
-        <Home />
-    </div>
-  );
-}
+const cartStore = new Cartstore();
+const productStore=new ProductStore();
+
+const App = observer(() => {
+    return (
+        <div className="App">
+            <Router>
+                <Routes>
+                    <Route path="/" element={<HomePage cartStore={cartStore} productStore={productStore}/>}/>
+                    <Route path="/cart" element={<CartPage cartStore={cartStore} productStore={productStore}/>}/>
+                </Routes>
+            </Router>
+
+        </div>
+    );
+})
 
 export default App;
