13e4e5b42b7bb92a92b46cc4ae4f63f8ef36df0c
parasmanchanda <paras.manchanda@noovosoft.com>
2024-09-06 19:36:05 +0530

    Created seperated files for typescript

---
+++ ReactExercise/react-product/src/types/addProductTypes.ts
@@ -0,0 +1,7 @@
+export interface IAddProductTypes {
+    product_name: string,
+    product_description: string,
+    product_price: number,
+    product_category: string,
+    product_discountPrice : number
+}
---
+++ ReactExercise/react-product/src/types/cartTypes.ts
@@ -0,0 +1,5 @@
+import {IProduct} from "./productTypes";
+export interface ICartType {
+    productDetail: IProduct,
+    isCart: boolean
+}
